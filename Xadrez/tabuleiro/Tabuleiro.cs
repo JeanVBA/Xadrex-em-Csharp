fcd1bff9c519d95764d9ba6ed7fe45ed4ccf1620
Jean Victor <jeanvictor999@outlook.com>
2023-07-16 18:52:32 -0300

    Tela vazia

--- Xadrez/tabuleiro/Tabuleiro.cs
+++ Xadrez/tabuleiro/Tabuleiro.cs
@@ -20,7 +20,10 @@ namespace Xadrez.tabuleiro
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
         }
-
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
+        }
 
     }
 }
